16ef956610b8faa21a5e535fb248f7a735e249fd
unknown <>
2021-07-25 20:09:13 -0400

    Updated api models

--- src/models/api/response.ts
+++ src/models/api/response.ts
@@ -1,4 +1,36 @@
-import { Feature } from "..";
+export type Feature = {
+  geometry?: any;
+  id?: string;
+  properties?: {
+    alert: any;
+    cdi: any;
+    code: string;
+    detail: string;
+    dmin: any;
+    felt: any;
+    gap: any;
+    ids: string;
+    mag: number;
+    magType: string;
+    mmi: any;
+    net: string;
+    nst: any;
+    place: string;
+    rms: number;
+    sig: number;
+    sources: string;
+    status: string;
+    time: number;
+    title: string;
+    tsunami: number;
+    type: string;
+    types: string;
+    tz: any;
+    updated: number;
+    url: string;
+  };
+  type?: string;
+};
 
 export type ApiResponse = {
   bbox?: any[];
