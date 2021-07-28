b447ac50af3868db8261bab441e9a013e2945918
unknown <>
2021-07-28 01:43:24 -0400

    Added map to event card

--- src/components/events/EventCard.tsx
+++ src/components/events/EventCard.tsx
@@ -8,35 +8,36 @@ interface EventCardProps {
 }
 
 const EventCard: React.FC<EventCardProps> = ({ feature }) => {
-  //   useEffect(() => {
-  //     let map: L.Map = L.map(`map_${id}`).setView([41, -10], 2);
-  //     // circle = L.circle([41, -10], { radius: 1000 * 1000 }).addTo(map);
-  //     // marker = L.marker([41, -10]).addTo(map);
-  //     // map.on("click", onMapClick);
+  // Load map after component renders
+  useEffect(() => {
+    var map = L.map(`mapid${feature.id}`).setView(
+      [feature.geometry["coordinates"][1], feature.geometry["coordinates"][0]],
+      6
+    );
+    L.tileLayer(
+      "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw",
+      {
+        attribution:
+          'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: "mapbox/streets-v11",
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken:
+          "pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw",
+      }
+    ).addTo(map);
 
-  //     L.tileLayer(
-  //       "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw",
-  //       {
-  //         attribution:
-  //           'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-  //         maxZoom: 18,
-  //         id: "mapbox/streets-v11",
-  //         tileSize: 512,
-  //         zoomOffset: -1,
-  //         accessToken:
-  //           "pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw",
-  //       }
-  //     ).addTo(map);
-  //     // eslint-disable-next-line react-hooks/exhaustive-deps
-  //   }, []);
+    // temp. TODO: update model
+    type adapter = typeof feature | any;
+    const feat: adapter = feature;
+    L.geoJSON(feat).addTo(map);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div className="card mx-2 my-2" style={{ width: "24rem" }}>
-      <img
-        src="https://m.media-amazon.com/images/I/61nTuLYI7pL._AC_SL1000_.jpg"
-        className="card-img-top"
-        alt=""
-      />
+      <div id={`mapid${feature.id}`} className="card-img-top card-map"></div>
       <div className="card-body text-dark">
         <h5 className="card-title">{feature.properties?.place}</h5>
         <p className="card-text">Magnitude: {feature.properties?.mag}</p>
