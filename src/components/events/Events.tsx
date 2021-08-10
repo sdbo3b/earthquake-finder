9e917a94bee1de9036b706c0f07ad5dbb287daa7
s <s@gmail.com>
2021-08-10 19:44:32 -0400

    Added page navigation for features

--- src/components/events/Events.tsx
+++ src/components/events/Events.tsx
@@ -9,6 +9,13 @@ const Events: React.FC = () => {
   const featureState = useAppSelector((state) => state.featureState.features);
   const [pageIndex, setPageIndex] = useState(0);
 
+  const numOfFeatures: number = featureState.length;
+  let numOfPages: number =
+    numOfFeatures <= 8
+      ? 1
+      : (numOfFeatures - (numOfFeatures % featuresPerPage)) / featuresPerPage;
+
+  // Calculates the appropriate amount of features per page
   const getFeatures = () => {
     let features;
 
@@ -21,33 +28,72 @@ const Events: React.FC = () => {
       <EventCard key={feature.id} feature={feature} />
     ));
   };
+
+  // Calculates the appropriate nav page numbers
+  const getPages = () => {
+    let pages = [pageIndex / 8];
+    if (numOfPages > 1) pages.push(pageIndex / 8 + 1);
+    if (numOfPages > 2) pages.push(pageIndex / 8 + 2);
+
+    return pages.map((page) => {
+      return (
+        <li
+          className={`page-item ${pageIndex / 8 === page && "active"} `}
+          aria-current="page"
+        >
+          <button className="page-link" onClick={(e) => setPageIndex(page * 8)}>
+            {page + 1}
+          </button>
+        </li>
+      );
+    });
+  };
+
+  const getNav = () => {
+    return (
+      <nav className="pt-3" aria-label="...">
+        <ul className="pagination">
+          <li className={`page-item ${pageIndex - 8 < 0 && "disabled"}`}>
+            <button
+              className="page-link"
+              tabIndex={-1}
+              aria-disabled="true"
+              onClick={() =>
+                setPageIndex((prevIndex) => {
+                  if (prevIndex - 8 < 0) return 0;
+                  return prevIndex - 8;
+                })
+              }
+            >
+              Previous
+            </button>
+          </li>
+          {getPages()}
+          <li className="page-item">
+            <button
+              className="page-link"
+              onClick={() =>
+                setPageIndex((prevIndex) => {
+                  if (prevIndex + 8 > featureState.length) return prevIndex;
+                  return prevIndex + 8;
+                })
+              }
+            >
+              Next
+            </button>
+          </li>
+        </ul>
+      </nav>
+    );
+  };
+
   return (
-    <div className="flex-grow-1 d-flex flex-column">
-      <div>
-        <button
-          onClick={() =>
-            setPageIndex((prevIndex) => {
-              if (prevIndex - 8 < 0) return 0;
-              return prevIndex - 8;
-            })
-          }
-        >
-          Back
-        </button>
-        <button
-          onClick={() =>
-            setPageIndex((prevIndex) => {
-              if (prevIndex + 8 > featureState.length) return prevIndex;
-              return prevIndex + 8;
-            })
-          }
-        >
-          Forward
-        </button>
-      </div>
+    <div className="flex-grow-1 d-flex flex-column primary-color justify-content-center align-items-center">
+      {getNav()}
       <div className="flex-grow-1 primary-color text-light row gy-0 gx-0 justify-content-center">
         {getFeatures()}
       </div>
+      {getNav()}
     </div>
   );
 };
